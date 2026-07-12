d17e1cc6d283639242506d2c3b264185826b52e8
ankush-kashyap <am6747379@gmail.com>
2026-07-12 23:35:28 +0530

    updating address section

--- src/components/checkout.jsx
+++ src/components/checkout.jsx
@@ -1,6 +1,23 @@
 import "./checkout.css";
+import { useState } from "react";
+
 
 function Checkout({ cartItems }) {
+
+  const [address, setAddress] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    address: "",
+    city: "",
+    state: "",
+    pincode: "",
+  });
+
+
+  const [addressType, setAddressType] = useState("Home");
+
+
   const totalPrice = cartItems.reduce(
     (total, item) => total + item.price * item.quantity,
     0
@@ -41,6 +58,111 @@ function Checkout({ cartItems }) {
             </div>
           ))
         )}
+
+
+        <div className="address-box">
+
+          <h2>Delivery Address</h2>
+
+          <input type="text"
+            placeholder="Full name"
+            value={address.name}
+            onChange={(e) =>
+              setAddress({ ...address, name: e.target.value })
+            }
+          />
+
+          <input
+            type="text"
+            placeholder="Phone Number"
+            value={address.phone}
+            onChange={(e) =>
+              setAddress({ ...address, phone: e.target.value })
+            }
+          />
+
+          <input
+            type="text"
+            placeholder="Email"
+            value={address.email}
+            onChange={(e) =>
+              setAddress({ ...address, email: e.target.value })
+            }
+          />
+
+          <textarea
+            placeholder="Address"
+            value={address.address}
+            onChange={(e) =>
+              setAddress({ ...address, address: e.target.value })
+            }
+          />
+
+          <div className="address-row">
+
+            <input
+              type="text"
+              placeholder="City"
+              value={address.city}
+              onChange={(e) =>
+                setAddress({ ...address, city: e.target.value })
+              }
+            />
+
+            <input
+              type="text"
+              placeholder="State"
+              value={address.state}
+              onChange={(e) =>
+                setAddress({ ...address, state: e.target.value })
+              }
+            />
+
+            <input
+              type="text"
+              placeholder="PIN Code"
+              value={address.pincode}
+              onChange={(e) =>
+                setAddress({
+                  ...address,
+                  pincode: e.target.value,
+                })
+              }
+            />
+          </div>
+
+          <div className="address-type">
+            <h3>Address Type</h3>
+
+            <label>
+              <input
+                type="radio"
+                name="addressType"
+                value="Home"
+                checked={addressType === "Home"}
+                onChange={(e) => setAddressType(e.target.value)}
+              />
+              Home
+            </label>
+
+            <label>
+              <input
+                type="radio"
+                name="addressType"
+                value="Work"
+                checked={addressType === "Work"}
+                onChange={(e) => setAddressType(e.target.value)}
+              />
+              Work
+            </label>
+          </div>
+
+
+
+        </div>
+
+
+
 
       </div>
 
