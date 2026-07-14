7cf78e0acc91a2482fc559f8057063fc54fb62de
ankush-kashyap <am6747379@gmail.com>
2026-07-14 10:31:10 +0530

    removing amazon link

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,9 +5,7 @@ function hero() {
             <div className="hero-content">
                 <h1>Big Deals on Electronics</h1>
                 <p>Shop the latest gadgets, fashion, and essentials.</p>
-                <button onClick={() =>
-                    window.open("https://www.amazon.com", "_blank")
-                }>Shop Now</button>
+                <button >Shop Now</button>
             </div>
         </section>
     );
